23809ee28c7c3b0aad6c164c3c07c55ac8d70c11
Omkar Karpe <omkarbkarpe02@gmail.com>
2024-04-07 14:21:06 +0530

    changed static root

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -117,16 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
-    os.path.join(BASE_DIR , 'staticfiles')
+    
 ]
-
 MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
-MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
