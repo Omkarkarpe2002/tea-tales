9d55b4792c2e4da672ec302557e12498c4b96eb4
Omkar Karpe <omkarbkarpe02@gmail.com>
2024-04-07 14:36:47 +0530

    changedstatic syntax

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -117,7 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -130,6 +129,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
