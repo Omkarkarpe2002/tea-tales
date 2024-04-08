87577eefb9bf24decea83ef016993e0ebbcc1af3
Omkar Karpe <omkarbkarpe02@gmail.com>
2024-04-08 18:10:58 +0530

    included staticfiles keyword

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"), 
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
-# STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
